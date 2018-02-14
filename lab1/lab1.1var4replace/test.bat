8727da2624dea08e045389629b2f1c23515d5cb7
Ilya Protasov <pfltriot@gmail.com>
2018-02-14 09:13:02 +0300

    add changes in test.bat

--- lab1/lab1.1var4replace/test.bat
+++ lab1/lab1.1var4replace/test.bat
@@ -1,53 +1,55 @@
+set PROGRAM="%~1"
+
 REM запуск программы без параметров
-lab1.1var4replace.exe
+%PROGRAM%
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM запуск программы с одним параметром
-lab1.1var4replace.exe OneParameter
+%PROGRAM% OneParameter
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM запуск программы без output
-lab1.1var4replace.exe input.txt "" "serched string" "rapleced string"
+%PROGRAM% input.txt "" "serched string" "rapleced string"
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM запуск программы без input
-lab1.1var4replace.exe "" out.txt "serched string" "rapleced string"
+%PROGRAM% "" out.txt "serched string" "rapleced string"
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM запуск программы без search string
-lab1.1var4replace.exe input.txt out.txt "" "rapleced string"
+%PROGRAM% input.txt out.txt "" "rapleced string"
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM запуск программы с 6 параметрами
-lab1.1var4replace.exe input.txt out.txt "searchString" "replaceString" "someString"
+%PROGRAM% input.txt out.txt "searchString" "replaceString" "someString"
 IF NOT ERRORLEVEL 1 GOTO err
 
 REM запуск программы с пустым файлом
-lab1.1var4replace.exe empty.txt out.txt "hello world" "goodbay world"
+%PROGRAM% empty.txt out.txt "hello world" "goodbay world"
 IF ERRORLEVEL 1 GOTO err
 FC /B out.txt outempty.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с 1-й строкой и 1-й заменой
-lab1.1var4replace.exe string-1-or-more-replace.txt out.txt "hello" "world"
+%PROGRAM% string-1-or-more-replace.txt out.txt "hello" "world"
 IF ERRORLEVEL 1 GOTO err
 FC /B out.txt outstring-1-or-more-replace1.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с 1-й строкой и множественными заменами
-lab1.1var4replace.exe string-1-or-more-replace.txt out.txt "ma" "mama"
+%PROGRAM% string-1-or-more-replace.txt out.txt "ma" "mama"
 IF ERRORLEVEL 1 GOTO err
 FC /B out.txt outstring-1-or-more-replace2.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с множеством строк и множеством замен
-lab1.1var4replace.exe multiline.txt out.txt "ma" "mama"
+%PROGRAM% multiline.txt out.txt "ma" "mama"
 IF ERRORLEVEL 1 GOTO err
 FC /B out.txt outmultiline1.txt
 IF ERRORLEVEL 1 GOTO err
 
 REM запуск программы с множеством строк search и replace параметры больше 1 слова 
-lab1.1var4replace.exe multiline.txt out.txt "Prince Vasili Kuragin" "Prince Kuragin"
+%PROGRAM% multiline.txt out.txt "Prince Vasili Kuragin" "Prince Kuragin"
 IF ERRORLEVEL 1 GOTO err
 FC /B out.txt outmultiline2.txt
 IF ERRORLEVEL 1 GOTO err
