4e8fab38b0a8ce0b329afefc492f60ad7060c23c
Ilya Protasov <pfltriot@gmail.com>
2018-03-21 08:25:54 +0300

    change name of test function

--- lab2/vector/vector_tests/VectorProcessorTests.cpp
+++ lab2/vector/vector_tests/VectorProcessorTests.cpp
@@ -24,30 +24,26 @@ BOOST_AUTO_TEST_SUITE(ProcessVector_function)
 		// должна умножить это число на само себя
 		BOOST_AUTO_TEST_CASE(should_mul_this_number_by_each_element)
 		{
-			vector<double> numbers = { -1};
+			vector<double> numbers = { -1 };
 			ProcessVector(numbers);
 
-			BOOST_CHECK(VectorsAreEqual( numbers, 
-				{ (-1 * -1) }
-			));
+			BOOST_CHECK(VectorsAreEqual(numbers,
+				{ (-1 * -1) }));
 		}
 	BOOST_AUTO_TEST_SUITE_END()
 
 	// при обработке вектора с несколькими элементами
 	BOOST_AUTO_TEST_SUITE(when_processing_a_vector_with_several_elements)
-		// должен умножить их минимальный элемент на каждый элемент вектора
-		BOOST_AUTO_TEST_CASE(should_mul_min_element_by_each_element_in_vector)
+		// должен умножить каждый элемент вектора на минимальный элемент
+		BOOST_AUTO_TEST_CASE(should_mul_each_element_in_vector_by_min_element)
 		{
 			vector<double> numbers = { -1.2, 1.2, 2.3, 3.2 };
 			ProcessVector(numbers);
 
 			const double MIN_NUMBER = -1.2;
 			BOOST_CHECK(VectorsAreEqual(numbers,
-			{ (-1.2 * MIN_NUMBER), (1.2 * MIN_NUMBER), (2.3 * MIN_NUMBER), (3.2 * MIN_NUMBER) }
-			));
+				{ (-1.2 * MIN_NUMBER), (1.2 * MIN_NUMBER), (2.3 * MIN_NUMBER), (3.2 * MIN_NUMBER) }));
 		}
 	BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE_END()
-
-
