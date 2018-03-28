a41df0e62e8dea7950b49992fc6bdc5759345402
Ilya Protasov <pfltriot@gmail.com>
2018-03-28 12:00:01 +0300

    #9 add fixture for checking tests

--- lab2/parseUrl/parseUrl_tests/URLParserTests.cpp
+++ lab2/parseUrl/parseUrl_tests/URLParserTests.cpp
@@ -3,98 +3,69 @@
 
 using namespace std;
 
+void CheckUrlParsing(const std::string& url, int expectedPort, const std::string& expectedDoc, const std::string& expecteddHost, bool expectedResult)
+{
+	Protocol protocol;
+	int port = 0;
+	string host = "";
+	string document = "";
+	BOOST_CHECK(expectedResult == ParseURL(url, protocol, port, host, document));
+	BOOST_CHECK_EQUAL(port, expectedPort);
+	BOOST_CHECK_EQUAL(document, expectedDoc);
+	BOOST_CHECK_EQUAL(host, expecteddHost);
+}
+
 BOOST_AUTO_TEST_SUITE(ParseURL_function)
 	// берет пустую строку и возвращает false
 	BOOST_AUTO_TEST_CASE(takes_empty_string_and_returns_false)
 	{
-		Protocol protocol;
-		int port;
-		string host;
-		string document;
-		BOOST_CHECK(!ParseURL("", protocol, port, host, document));
+		CheckUrlParsing("", 0, "", "", false);
 	}
 
 	// берет неверный url и возвращает false
 	BOOST_AUTO_TEST_CASE(takes_invalid_url_and_returns_false)
 	{
-		Protocol protocol;
-		int port;
-		string host;
-		string document;
-		BOOST_CHECK(!ParseURL("http:/www.mysite.com/docs", protocol, port, host, document));
-		BOOST_CHECK(!ParseURL("https//www.mysite.com/docs", protocol, port, host, document));
-		BOOST_CHECK(!ParseURL("ftpwww.mysite.com/docs", protocol, port, host, document));
+		CheckUrlParsing("http:/www.mysite.com/docs", 0, "", "", false);
+		CheckUrlParsing("https//www.mysite.com/docs", 0, "", "", false);
+		CheckUrlParsing("ftpwww.mysite.com/docs", 0, "", "", false);
 	}
 
 	// берет url без протокола или с неверным протоколом и возвращает false
 	BOOST_AUTO_TEST_CASE(takes_url_without_protocol_or_with_invalid_protocol_and_returns_false)
 	{
-		Protocol protocol;
-		int port;
-		string host;
-		string document;
-		BOOST_CHECK(!ParseURL("://www.mysite.com/docs", protocol, port, host, document));
-		BOOST_CHECK(!ParseURL("httpss://www.mysite.com/docs", protocol, port, host, document));
-		BOOST_CHECK(!ParseURL("ftps://www.mysite.com/docs", protocol, port, host, document));
+		CheckUrlParsing("://www.mysite.com/docs", 0, "", "", false);
+		CheckUrlParsing("httpss://www.mysite.com/docs", 0, "", "", false);
+		CheckUrlParsing("ftps://www.mysite.com/docs", 0, "", "", false);
 	}
 
 	// берет url без хоста или с неверным хостом и возвращает false
 	BOOST_AUTO_TEST_CASE(takes_url_without_host_or_with_invalid_host_and_returns_false)
 	{
-		Protocol protocol;
-		int port;
-		string host;
-		string document;
-		BOOST_CHECK(!ParseURL("http://www__mysite_com/docs", protocol, port, host, document));
-		BOOST_CHECK(!ParseURL("https://www.?%$mysite.com/docs", protocol, port, host, document));
-		BOOST_CHECK(!ParseURL("ftp:///docs/document1.html", protocol, port, host, document));
+		CheckUrlParsing("http://www__mysite_com/docs", 0, "", "", false);
+		CheckUrlParsing("https://www.?%$mysite.com/docs", 0, "", "", false);
+		CheckUrlParsing("ftp:///docs/document1.html", 0, "", "", false);
 	}
 
 	// берет корректный url, парсит его и возвращает true
 	BOOST_AUTO_TEST_CASE(takes_correct_url_parses_it_and_returns_true)
 	{
-		Protocol protocol;
-		int port;
-		string host;
-		string document;
-		BOOST_CHECK(ParseURL("Http://www.mysite.com:123/docs", protocol, port, host, document));
-		BOOST_CHECK_EQUAL(port, 123);
-		BOOST_CHECK_EQUAL(document, "docs");
-		BOOST_CHECK_EQUAL(host, "www.mysite.com");
-		BOOST_CHECK(ParseURL("hTTps://www.mysite.com:1/docs", protocol, port, host, document));
-		BOOST_CHECK_EQUAL(port, 1);
-		BOOST_CHECK_EQUAL(document, "docs");
-		BOOST_CHECK_EQUAL(host, "www.mysite.com");
-		BOOST_CHECK(ParseURL("FTP://www.mysite.com:65535/docs", protocol, port, host, document));
-		BOOST_CHECK_EQUAL(port, 65535);
-		BOOST_CHECK_EQUAL(document, "docs");
-		BOOST_CHECK_EQUAL(host, "www.mysite.com");
+		CheckUrlParsing("Http://www.mysite.com:123/docs", 123, "docs", "www.mysite.com", true);
+		CheckUrlParsing("hTTps://www.mysite.com:1/docs", 1, "docs", "www.mysite.com", true);
+		CheckUrlParsing("FTP://www.mysite.com:65535/docs", 65535, "docs", "www.mysite.com", true);
 	}
 
 	// берет url без явного указания порта и возвращает true
 	BOOST_AUTO_TEST_CASE(takes_url_without_explicitly_specifying_port_and_returns_true)
 	{
-		Protocol protocol;
-		int port;
-		string host;
-		string document;
-		BOOST_CHECK(ParseURL("FTP://www.mysite.com/docs", protocol, port, host, document));
-		BOOST_CHECK_EQUAL(port, 21);
-		BOOST_CHECK(ParseURL("HttPs://www.mysite.com/docs", protocol, port, host, document));
-		BOOST_CHECK_EQUAL(port, 443);
-		BOOST_CHECK(ParseURL("http://www.mysite.com/docs", protocol, port, host, document));
-		BOOST_CHECK_EQUAL(port, 80);
+		CheckUrlParsing("FTP://www.mysite.com/docs", 21, "docs", "www.mysite.com", true);
+		CheckUrlParsing("HttPs://www.mysite.com/docs", 443, "docs", "www.mysite.com", true);
+		CheckUrlParsing("http://www.mysite.com/docs", 80, "docs", "www.mysite.com", true);
 	}
 
 	// берет url без документа и возвращает true
 	BOOST_AUTO_TEST_CASE(takes_url_without_document_and_returns_true)
 	{
-		Protocol protocol;
-		int port;
-		string host;
-		string document;
-		BOOST_CHECK(ParseURL("http://www.mysite.com", protocol, port, host, document));
-		BOOST_CHECK_EQUAL(document, "");
+		CheckUrlParsing("http://www.mysite.com", 80, "", "www.mysite.com", true);
 	}
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -107,10 +78,10 @@ BOOST_AUTO_TEST_SUITE(CheckProtocol_function)
 	// берет строку протокола и возвращает его тип
 	BOOST_AUTO_TEST_CASE(takes_protocol_string_and_returns_its_type)
 	{
-		BOOST_CHECK(AreProtocolsEquals(CheckProtocol("http"), Protocol::HTTP));
-		BOOST_CHECK(AreProtocolsEquals(CheckProtocol("https"), Protocol::HTTPS));
-		BOOST_CHECK(AreProtocolsEquals(CheckProtocol("ftp"), Protocol::FTP));
-		BOOST_CHECK(AreProtocolsEquals(CheckProtocol("invalid"), Protocol::NotValid));
+		BOOST_CHECK(CheckProtocol("http") == Protocol::HTTP);
+		BOOST_CHECK(CheckProtocol("https") == Protocol::HTTPS);
+		BOOST_CHECK(CheckProtocol("ftp") == Protocol::FTP);
+		BOOST_CHECK(CheckProtocol("invalid") == Protocol::NotValid);
 	}
 BOOST_AUTO_TEST_SUITE_END()
 
