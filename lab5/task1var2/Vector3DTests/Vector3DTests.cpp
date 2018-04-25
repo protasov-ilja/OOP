c036401007af9f956e64d24ee567c4b6effd81ed
Ilya Protasov <pfltriot@gmail.com>
2018-04-25 08:30:10 +0300

    renamed tests and added tests for << >> * operators and Normalize func

--- lab5/task1var2/Vector3DTests/Vector3DTests.cpp
+++ lab5/task1var2/Vector3DTests/Vector3DTests.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "..\CVector3D.h"
+#include <sstream>
 
 const double EPSILON = 0.001;
 
@@ -18,43 +19,53 @@ bool compareVectors3D(const CVector3D& vector1, const CVector3D& vector2)
 TEST_CASE("test vector3D")
 {
 	// может создать свой экземпляр с нулевыми параметрами
-	SECTION("vector1")
+	SECTION("can create its own instance with zero parameters")
 	{
 		CVector3D vector;
 		REQUIRE(compareVectors3D(vector, { 0, 0, 0 }));
 	}
 
-	// может создать свой экземпляр с задаными параметрами
-	SECTION("vector2")
+	// может создать свой экземпляр с заданными параметрами
+	SECTION("can create its own instance with the specified parameters")
 	{
 		CVector3D vector(2, 4, 5);
 		REQUIRE(compareVectors3D(vector, {2, 4, 5}));
 	}
 
 	// может вернуть свою длину
+	SECTION("can return its length")
 	{
 		CVector3D vector(2, 2, 1);
 		REQUIRE(CompareDoubles(vector.GetLength(), sqrt(2 * 2 + 2 * 2 + 1 * 1)));
 	}
 
-	// может не менять своих данных при использовании унарного плюса
-	SECTION("vector2.2")
+	// может нормализовать себя
+	SECTION("can normalize itself")
+	{
+		CVector3D vector(2, 2, 1);
+		double lengthInversion = 1 / vector.GetLength();
+		vector.Normalize();
+		REQUIRE(compareVectors3D(vector, { (2 * lengthInversion) , (2 * lengthInversion), (1 * lengthInversion) }));
+	}
+
+	// не меняет своих данных при использовании унарного плюса
+	SECTION("doesnt change its data when using the unary plus")
 	{
 		CVector3D vector(2, 4, 5);
 		CVector3D vector1 = +vector;
 		REQUIRE(compareVectors3D(vector1, { 2, 4, 5 }));
 	}
 
-	// может поменять свои параметры на противоположные при использовании унарного минуса
-	SECTION("vector2.3")
+	// может изменить свои параметры на противоположные при использовании унарного минуса
+	SECTION("can change its parameters to the opposite when using the unary minus")
 	{
 		CVector3D vector(2, 4, 5);
 		CVector3D vector1 = -vector;
 		REQUIRE(compareVectors3D(vector1, { -2, -4, -5 }));
 	}
 
-	//  может сложить длину второго вектора с длиной первого
-	SECTION("vector3")
+	// может сложить длину второго вектора с длиной первого
+	SECTION("can add the length of the second vector with the length of the first")
 	{
 		CVector3D vector1(2, 4.4, 5);
 		CVector3D vector2(0, 0, 0);
@@ -63,7 +74,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// может вычесть длину второго вектора из длины первого
-	SECTION("vector4")
+	SECTION("can subtract the length of the second vector from the length of the first")
 	{
 		CVector3D vector1(2, 4.4, 5);
 		CVector3D vector2(0, 0, 0);
@@ -71,8 +82,8 @@ TEST_CASE("test vector3D")
 		REQUIRE(compareVectors3D(subVector, { 2, 4.4, 5 }));
 	}
 
-	// может вернуть результат увеличения своей длины на длинну второго вектора
-	SECTION("vector5")
+	// может вернуть результат увеличения своей длины на длину второго вектора
+	SECTION("can return the result of increasing its length by the length of the second vector")
 	{
 		CVector3D vector1(2, 4.4, 5);
 		CVector3D vector2(0, 0, 0);
@@ -81,7 +92,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// может вернуть результат уменьшения своей длины на длинну второго вектора
-	SECTION("vector6")
+	SECTION("can return the result of decreasing its length by the length of the second vector")
 	{
 		CVector3D vector1(2, 4.4, 5);
 		CVector3D vector2(0, 0, 0);
@@ -90,7 +101,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// может вернуть результат умножения своей длины на скаляр
-	SECTION("vector7")
+	SECTION("can return the result of multiplying its length by scalar")
 	{
 		double scalar = 2;
 		CVector3D vector1(1, 2, 3);
@@ -99,7 +110,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// может вернуть результат умножения скаляра на свою длину 
-	SECTION("vector8")
+	SECTION("can return the result of multiplying the scalar by its length")
 	{
 		double scalar = 2;
 		CVector3D vector1(1, 2, 3);
@@ -108,7 +119,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// может вернуть результат деления своей длины на скаляр
-	SECTION("vector9")
+	SECTION("can return the result of dividing its length by a scalar")
 	{
 		double scalar = 2;
 		CVector3D vector1(2, 2, 2);
@@ -117,7 +128,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// может умножить свою длинну на скаляр
-	SECTION("vector10")
+	SECTION("can multiply its length by scalar")
 	{
 		double scalar = 2;
 		CVector3D vector(1, 2, 3);
@@ -126,7 +137,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// может разделить свою длинну на скаляр
-	SECTION("vector11")
+	SECTION("can divide its length by scalar")
 	{
 		double scalar = 2;
 		CVector3D vector(2, 2, 2);
@@ -135,7 +146,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// при сравнении двух векторов возвращает истину при их равенстве
-	SECTION("vector12")
+	SECTION("when comparing two vectors returns true when they are equal")
 	{
 		CVector3D vector1(2, 2, 2);
 		CVector3D vector2(2, 2, 2);
@@ -143,7 +154,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// при сравнении двух векторов возвращает ложь при их не равенстве
-	SECTION("vector12")
+	SECTION("when comparing two vectors returns false if they do not equal")
 	{
 		CVector3D vector1(2, 2, 2);
 		CVector3D vector2(2, 3, 2);
@@ -151,7 +162,7 @@ TEST_CASE("test vector3D")
 	}
 
 	// при сравнении двух векторов возвращает истину при их не равенстве
-	SECTION("vector12")
+	SECTION("when comparing two vectors returns true if they do not equal")
 	{
 		CVector3D vector1(2, 2, 2);
 		CVector3D vector2(2, 3, 2);
@@ -159,10 +170,28 @@ TEST_CASE("test vector3D")
 	}
 
 	// при сравнении двух векторов возвращает ложь при их равенстве
-	SECTION("vector12")
+	SECTION("when comparing two vectors returns false if they are equal")
 	{
 		CVector3D vector1(2, 2, 2);
 		CVector3D vector2(2, 2, 2);
 		REQUIRE(!(vector1 != vector2));
+	}
+
+	// может считывать вектор из потока
+	SECTION("can read a vector from the stream")
+	{
+		std::stringstream strm("2, 2, 2");
+		CVector3D vector;
+		strm >> vector;
+		REQUIRE(compareVectors3D(vector, { 2, 2, 2 }));
+	}
+
+	// может записать вектор в поток
+	SECTION("can write a vector into a stream")
+	{
+		std::stringstream strm;
+		CVector3D vector(1, 2, 3);
+		strm << vector;
+		REQUIRE(strm.str() == "1, 2, 3");
 	}
 }
